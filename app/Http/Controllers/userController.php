0b13b358af30ce648e6ac46db546941053681735
SonicEz99 <pnem2003@gmail.com>
2024-05-12 22:41:20 +0700

    feat : [Update] table : User

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -7,6 +7,26 @@ use Illuminate\Http\Request;
 
 class userController extends Controller
 {
+    public function register(Request $request){
+
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string',
+            'pass' => 'required|min:8',
+        ]);
+
+        $user = new User;
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->pass;
+        $user->save();
+
+        //$user->fill($request->all());
+
+        return  redirect()->route('user.data')->with(['success' => 'User has been saved!']);    
+    }
+    
     public function showData(){
         $users = User::all();
 
@@ -18,4 +38,28 @@ class userController extends Controller
 
         return redirect()->route('user.data')->with(['delete' => 'User has been deleted!']);
     }
+
+    public function showEdit($id){
+        $user = User::find($id);
+
+        return view('update', compact('user'));
+    }
+
+    public function update(Request $request, $id){
+
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string',
+        ]);
+    
+        $user = User::find($id);
+        $user->fill($request->except('pass')); // Exclude the 'pass' field from the update
+        $user->save();
+    
+        return redirect()->route('user.data')->with(['success' => 'User has been updated!']);    
+    
+    }
+    
+    
+    
 }
